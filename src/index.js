3ce456d7783b1113fdd77ed1f8ee784917cc85cb
Cassel-Evens <casselke@gmail.com>
2022-01-23 01:04:08 -0800

    add updated status to overall project

--- src/index.js
+++ src/index.js
@@ -4,10 +4,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeCurrency from './currency.js';
 
-function clearField() {
-  $("#US").val("");
-}
-
 $(document).ready(function() {
   $("#formOne").submit(function() {
     event.preventDefault();
@@ -18,9 +14,6 @@ $(document).ready(function() {
       let body = JSON.parse(response);
       let conversionRate = body.conversion_rates[currency];
       $('#result').text(`${total} USD = ${conversionRate * total} ${currency}`);
-    }).catch(function(response) {
-      $('#showError').text(`There was an error with the request. Status: ${response.status}`);
     });
   });
-  clearField();
 });

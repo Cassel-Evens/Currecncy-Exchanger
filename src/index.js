ea388d3930bd9e66d68d42d5a0524cc0f14fc572
Cassel-Evens <casselke@gmail.com>
2022-01-23 00:27:00 -0800

    add updated currency.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeCurrency from './currency';
+
+// function clearField() {
+//   $("#US").val("");
+// }
+
+$(document).ready(function() {
+  $("#formOne").submit(function() {
+    event.preventDefault();
+    let total = $('#US').val();
+    let currency = $('#Currency').val();
+
+    (async () => {
+      let currencyExchange = new CurrencyExchange();
+      const response = await currencyExchange.getCurrency();
+      getConversion(response);
+    })();
+  });
+});

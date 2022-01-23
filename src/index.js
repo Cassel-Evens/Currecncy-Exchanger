e5cc816e961e91510224af0b1350d7661abe2326
Cassel-Evens <casselke@gmail.com>
2022-01-23 00:50:33 -0800

    add updated index.js as well as updated HTML css

--- src/index.js
+++ src/index.js
@@ -2,22 +2,25 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import ExchangeCurrency from './currency';
+import ExchangeCurrency from './currency.js';
 
-// function clearField() {
-//   $("#US").val("");
-// }
+function clearField() {
+  $("#US").val("");
+}
 
 $(document).ready(function() {
   $("#formOne").submit(function() {
     event.preventDefault();
     let total = $('#US').val();
-    let currency = $('#Currency').val();
-
-    (async () => {
-      let currencyExchange = new CurrencyExchange();
-      const response = await currencyExchange.getCurrency();
-      getConversion(response);
-    })();
+    let currency = $('#moneyExchange').val();
+    let promise = ExchangeCurrency.getExchange();
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      let conversionRate = body.conversion_rates[currency];
+      $('#result').text(`${total} USD = ${conversionRate * total} ${currency}`);
+    }).catch(function(response) {
+      $('#showError').text(`There was an error with the request. Status: ${response.status}`);
+    });
   });
+  clearField();
 });

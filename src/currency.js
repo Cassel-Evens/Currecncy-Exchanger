d1d505e7396e6961b5b2c3771a6a29bfe390d27d
Cassel-Evens <casselke@gmail.com>
2022-01-23 10:34:18 -0800

    add re workerd project

--- src/currency.js
+++ src/currency.js
@@ -1,17 +1,13 @@
 export default class ExchangeCurrency {
-  static getExchange() {
-    return new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request);
+  static async getExchange(total, currency) {
+    try {
+      const response = await fetch (`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      if (!response.ok) {
+        throw Error(response.statusText);
         }
-      };
-      request.open("GET", url, true);
-      request.send();
-    });
-  }
+        return response.json();
+      } catch(error) {
+        return error.message;
+      }
+    }
 }
--- src/index.js
+++ src/index.js
@@ -4,16 +4,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeCurrency from './currency.js';
 
+function getElements(response) {
+  if(response.main) {
+    $('#result').text(`The exchange rate is ${response.main.conversion_rates}`);
+  } else {
+    $('#showErrors').text(`There was an error: ${response}`);
+  }
+}
+
+// async function makeApiCall(total, currency) {
+//   const response = await ExchangeCurrency.getExchange(total, currency);
+//   getElements(response);
+// }
+
 $(document).ready(function() {
   $("#formOne").submit(function() {
     event.preventDefault();
     let total = $('#US').val();
     let currency = $('#moneyExchange').val();
-    let promise = ExchangeCurrency.getExchange();
-    promise.then(function(response) {
-      let body = JSON.parse(response);
-      let conversionRate = body.conversion_rates[currency];
-      $('#result').text(`${total} USD = ${conversionRate * total} ${currency}`);
-    });
+    // clearFields();
+    (async function() {
+      const response = await ExchangeCurrency.getExchange(total, currency);
+      getElements(response);
+    })();
   });
 });
